53062a5bfb46f11caecd945295c421b438f4e142
Gabriel Zarate <admin@Admins-MacBook-Pro.local>
2018-12-26 07:22:11 -0500

    learning go basics

---
+++ src/range/range.go
@@ -0,0 +1,44 @@
+package main 
+
+import "fmt"
+
+func main() {
+  
+  nums := []int{2, 3, 4}
+  sum := 0
+  for _, num := range nums {
+    sum += num
+  }
+
+  fmt.Println("sum:", sum)
+  // sum: 9
+
+  for i, num := range nums {
+    if num == 3 {
+      fmt.Println("index:", i)
+    }
+  }
+  // index: 1
+
+  kvs := map[string]string{ "a": "apple", "b": "banana"}
+  for k, v := range kvs {
+    fmt.Println("%s -> %s\n", k, v)
+  }
+  // %s -> %s
+  // a apple
+  // %s -> %s
+  // b banana
+
+  for k := range kvs {
+    fmt.Println("key", k)
+  }
+  // key: a
+  // key: b
+
+  for i, c := range "go" {
+    fmt.Println(i, c)
+  }
+  // 0 103
+  // 1 111
+
+}
